a3e3b51468afef73405a0a048699721d182e082c
PasinduKavishka1 <100836882+PasinduKavishka1@users.noreply.github.com>
2023-01-30 17:08:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,32 +2,32 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
-global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.botname = "🌹Queen Anya🐦" 
+global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 || 𝗦𝗶𝗻𝗵𝗮𝗹𝗮 \\
+global.author = "卩卂丂丨几ᗪㄩ"  
+global.packname = "🥵𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.myweb = "https://github.com/pkavishka/Anya-pika-MD-v2" // You can put any link 
+global.waterMark = "×͜× 🥵King ✘ ᴘᴀꜱɪɴᴅᴜ💫™ ✓" // Your own watermark 
+global.headerText = "Anya by Pasindu" // text for header of menu         
+global.DocumentTitle = "🥵MASTER⚔️𝗣𝗔𝗦𝗜𝗡𝗗𝗨 🦄" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
+global.ownername = "⚔️𝗣𝗔𝗦𝗜𝗡𝗗𝗨🥵"   // Owner name //        
+global.ownernumber = ['94767120549'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
+global.ytname = "YT: 𝗣𝗮𝘀𝗶𝗻𝗱𝘂"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
+global.region = "Sri Lanka" // your country name
+global.state = "Sri Lanka" // your state name
+global.district = "👀" // your district name
+global.timezone = "Asia/Colombo" // search on google if you don't know the timezone of your country //
+global.instagramId = "⚔️𝗣𝗔𝗦𝗜𝗡𝗗𝗨🥵" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.premium = ['94767120549']  // available soon
+global.sudo = ['94767120549']  // Co - Owners //
 
 global.prefa = ['-','🥵','🔥']
 
